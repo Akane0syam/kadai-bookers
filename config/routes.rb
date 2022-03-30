ef7850ac38ed7e68499e5ad4ce99f73ecf2ce3f3
Akane0syam <11kangae096@gmail.com>
2022-03-30 14:05:39 +0000

    [Update]詳細画面を作る->createアクションのredirect_toを変更した

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/top' => 'books#top'
+  root :to => 'books#top'
   get 'books/new'
   post 'books' => 'books#create'
-  get '/index' => 'books#index'
-  get '/show' => 'books#show'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  #'books#show'の設定を、bookとして利用できる
   get '/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
