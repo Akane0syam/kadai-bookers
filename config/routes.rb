9c01a0336fdcbcde300a90490726ca7f52244336
Akane0syam <11kangae096@gmail.com>
2022-03-31 14:52:13 +0000

    [Add]HTMLファイルを編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get '/top' => 'books#top'
   root :to => 'books#top'
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  #'books#show'の設定を、bookとして利用できる
-  get '/edit' => 'books#edit'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
