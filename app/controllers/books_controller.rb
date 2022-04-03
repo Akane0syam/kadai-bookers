e097f3eeb33b594fa96b459c7ad3da5e97be23b3
Akane0syam <11kangae096@gmail.com>
2022-04-03 21:14:32 +0000

    [Add]サクセスメッセージを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
       @books = Book.all
       render :new
@@ -32,13 +32,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: "Book was successfully updated."
   end
 
   def destroy
     book = Book.find(params[:id])# データ（レコード）を1件取得
     book.destroy # データ（レコード）を削除
-    redirect_to '/books'# 投稿一覧画面へリダイレクト
+    redirect_to '/books', notice: "Book was successfully destroyed."# 投稿一覧画面へリダイレクト
   end
 
   private
